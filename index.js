2ed2384a2163f132872308d3639faff82cfc8d5e
ktadewos <kaleab.ero@mediaagility.com>
2021-08-20 11:35:51 -0500

    solve port in use issue for multiple test suites

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-const mongoose = require('mongoose');
-const config = require('config'); 
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const adminRole = require('./middleware/adminRole');
-const auth = require('./middleware/auth');
-const ErrorResponse = require('./models/errorResponse');
+const app = require('./server');
 
-const personRoutes = require('./routes/person.route');
-const userRoutes = require('./routes/user.route');
-const app = express();
-require('./startup/db')();
-
-app.use(express.json());
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/api', userRoutes);
-app.use('/api', [auth, adminRole], personRoutes);
-
-app.get('/', (req, res, next) => {
-    res.send('Hello from Node Application');
-});
-
-app.use((err, req, res, next) => {
-    return res.status(500).send(new ErrorResponse(500, err));
-});
-
-const server = app.listen(3000, () => console.log('connected'));
-
-
-module.exports = server;
+app.listen(3000, () => console.log('connected'));
---
+++ server.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const config = require('config'); 
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const adminRole = require('./middleware/adminRole');
+const auth = require('./middleware/auth');
+const ErrorResponse = require('./models/errorResponse');
+
+const personRoutes = require('./routes/person.route');
+const userRoutes = require('./routes/user.route');
+const app = express();
+require('./startup/db')();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', userRoutes);
+app.use('/api', [auth, adminRole], personRoutes);
+
+app.get('/', (req, res, next) => {
+    res.send('Hello from Node Application');
+});
+
+app.use((err, req, res, next) => {
+    return res.status(500).send(new ErrorResponse(500, err));
+});
+
+
+module.exports = app;
