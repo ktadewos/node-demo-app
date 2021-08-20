155b2856db0d8a990965db9fc3a7e80ceb5906e4
ktadewos <kaleab.ero@mediaagility.com>
2021-08-20 14:48:04 -0500

    refactor unit and integration test code

--- test/unit/authentication.controller.test.js
+++ test/unit/authentication.controller.test.js
@@ -3,94 +3,63 @@ const ApiResponse = require('../../models/apiResponse');
 const ErrorResponse = require('../../models/errorResponse');
 const { User } = require('../../models/user');
 const bcrypt = require('bcrypt');
+let resMock;
+let user;
+let generatedToken;
 
 describe('Authentication Controller Test Cases', () => {
-    it('should respond valid error response with 403 status code when user not found', async () => {
-
-        const user = null;
-        const apiResponse = new ErrorResponse('403', 'user or password is not correct');
+    
+    beforeAll(() => {
         
-        const req = {
-            body: {
-                email: 'x',
-                password: '$2a$12$o/WVefB23cyX.fdsa4XEG.d5yhHk1phWxd/fLB//zsBZNfsQYK4Mq' //bcrypt encrypt for 123.
-            }
-        };
-        const resMock = {
+        generatedToken = 'token'
+        
+        mockResponse = {
             status: jest.fn().mockReturnThis(),
-            send: jest.fn()
+            send: jest.fn(),
+            header: jest.fn().mockReturnThis()
         };
-        const next = jest.fn();
-    
-        User.findOne = jest.fn().mockResolvedValue(user);
-        
-        await authenticationController.login(req, resMock, next);
-    
-        expect(resMock.status).toBeCalledWith(403);
-        expect(resMock.send).toBeCalledWith(apiResponse);
-        expect(User.findOne).toHaveBeenCalled();
-     });
 
-     it('should respond valid error response with 403 status code when user password is not correct', async () => {
-
-        const user = {
-            email: 'x',
-            password: '$2a$12$Kd6G1lInS07.krwiHtrWgODfpd/p/bmDve8BdAMk1Wb1hje4kvl5i' //bcrypt encrypt for 1234.
-        };
-        const apiResponse = new ErrorResponse('403', 'user or password is not correct');
-        
-        const req = {
-            body: {
-                email: 'x',
-                password: '$2a$12$o/WVefB23cyX.fdsa4XEG.d5yhHk1phWxd/fLB//zsBZNfsQYK4Mq' //bcrypt encrypt for 123.
-            }
-        };
-        const resMock = {
-            status: jest.fn().mockReturnThis(),
-            send: jest.fn()
-        };
-        const next = jest.fn();
-
-        bcrypt.compare = jest.fn().mockImplementation((x,y) => {
-            if (x == y) return Promise.resolve(true);
-            return Promise.resolve(false);
-        });
-    
-        User.findOne = jest.fn().mockResolvedValue(user);
-        
-        await authenticationController.login(req, resMock, next);
-    
-        expect(resMock.status).toBeCalledWith(403);
-        expect(resMock.send).toBeCalledWith(apiResponse);
-        expect(User.findOne).toHaveBeenCalled();
-     });
-
-     it('should respond valid response with 200 status code when user email and password are valid', async () => {
-
-        
-        const generatedToken = 'token'
-
-        const user = {
+        user = {
             email: 'x',
             password: '$2a$12$o/WVefB23cyX.fdsa4XEG.d5yhHk1phWxd/fLB//zsBZNfsQYK4Mq', //bcrypt encrypt for 123.
             generateAuthToken: jest.fn().mockImplementation(() => {
                 return generatedToken;
             })
         };
+    });
+    
+    it('should respond valid error response with 403 status code when user not found', async () => {
 
-        const apiResponse = new ApiResponse(200, 'success', generatedToken);
+        const apiResponse = new ErrorResponse('403', 'user or password is not correct');
+        
+        const req = {
+            body: {
+                email: 'invalid email'
+            }
+        };
+        
+        const next = jest.fn();
+    
+        User.findOne = jest.fn().mockResolvedValue(null);
+        
+        await authenticationController.login(req, mockResponse, next);
+    
+        expect(mockResponse.status).toBeCalledWith(403);
+        expect(mockResponse.send).toBeCalledWith(apiResponse);
+        expect(User.findOne).toHaveBeenCalled();
+     });
+
+     it('should respond valid error response with 403 status code when user password is not correct', async () => {
+
+        const apiResponse = new ErrorResponse('403', 'user or password is not correct');
         
         const req = {
             body: {
                 email: 'x',
-                password: '$2a$12$o/WVefB23cyX.fdsa4XEG.d5yhHk1phWxd/fLB//zsBZNfsQYK4Mq' //bcrypt encrypt for 123.
+                password: '$2a$12$Kd6G1lInS07.krwiHtrWgODfpd/p/bmDve8BdAMk1Wb1hje4kvl5i' //bcrypt encrypt for 1234.
             }
         };
-        const resMock = {
-            status: jest.fn().mockReturnThis(),
-            send: jest.fn(),
-            header: jest.fn().mockReturnThis()
-        };
+        
         const next = jest.fn();
 
         bcrypt.compare = jest.fn().mockImplementation((x,y) => {
@@ -100,12 +69,40 @@ describe('Authentication Controller Test Cases', () => {
     
         User.findOne = jest.fn().mockResolvedValue(user);
         
-        await authenticationController.login(req, resMock, next);
+        await authenticationController.login(req, mockResponse, next);
     
-        expect(resMock.status).toBeCalledWith(200);
-        expect(resMock.send).toBeCalledWith(apiResponse);
+        expect(mockResponse.status).toBeCalledWith(403);
+        expect(mockResponse.send).toBeCalledWith(apiResponse);
+        expect(User.findOne).toHaveBeenCalled();
+     });
+
+     it('should respond valid response with 200 status code when user email and password are valid', async () => {
+
+       const apiResponse = new ApiResponse(200, 'success', generatedToken);
+        
+        const req = {
+            body: {
+                email: 'x',
+                password: '$2a$12$o/WVefB23cyX.fdsa4XEG.d5yhHk1phWxd/fLB//zsBZNfsQYK4Mq' //bcrypt encrypt for 123.
+            }
+        };
+
+        const next = jest.fn();
+
+        bcrypt.compare = jest.fn().mockImplementation((x,y) => {
+            if (x == y) return Promise.resolve(true);
+            return Promise.resolve(false);
+        });
+    
+        User.findOne = jest.fn().mockResolvedValue(user);
+        
+        await authenticationController.login(req, mockResponse, next);
+    
+        expect(mockResponse.status).toBeCalledWith(200);
+        expect(mockResponse.status).toBeCalledTimes(3);
+        expect(mockResponse.send).toBeCalledWith(apiResponse);
         expect(user.generateAuthToken).toBeCalled();
-        expect(resMock.header).toBeCalledWith('x-auth-token', generatedToken);
+        expect(mockResponse.header).toBeCalledWith('x-auth-token', generatedToken);
         expect(User.findOne).toHaveBeenCalledWith({ email: req.body.email });
      });
 })
--- test/unit/person.controller.test.js
+++ test/unit/person.controller.test.js
@@ -2,28 +2,33 @@ const personController = require('../../controller/person.controller');
 const { Person } = require('../../models/person');
 const ApiResponse = require('../../models/apiResponse');
 const ErrorResponse = require('../../models/errorResponse');
+let mockResponse;
 
 
 
 describe('Person Controller Test Cases', () => {
+    
+    beforeAll(() => {
+        mockResponse = {
+            status: jest.fn().mockReturnThis(),
+            send: jest.fn()
+        };
+    })
+    
     it('should send valid error response and status code if person not found', async () => {
 
         const person = null;
         const apiResponse = new ErrorResponse('204', 'no content found!');
         
         const req = {};
-        const resMock = {
-            status: jest.fn().mockReturnThis(),
-            send: jest.fn()
-        };
         const next = jest.fn();
     
         Person.find = jest.fn().mockResolvedValue(person);
         
-        await personController.findAll(req, resMock, next);
+        await personController.findAll(req, mockResponse, next);
     
-        expect(resMock.status).toBeCalledWith(204);
-        expect(resMock.send).toBeCalledWith(apiResponse);
+        expect(mockResponse.status).toBeCalledWith(204);
+        expect(mockResponse.send).toBeCalledWith(apiResponse);
         expect(Person.find).toHaveBeenCalled();
         //expect.assertions(3) //can be used to check x times assertion made for asynchrnous functions test
      });
@@ -34,18 +39,14 @@ describe('Person Controller Test Cases', () => {
         const apiResponse = new ApiResponse(200, 'success', person)
         
         const req = {};
-        const resMock = {
-            status: jest.fn().mockReturnThis(),
-            send: jest.fn()
-        };
         const next = jest.fn();
     
         Person.find = jest.fn().mockResolvedValue(person);
         
-        await personController.findAll(req, resMock, next);
+        await personController.findAll(req, mockResponse, next);
     
-        expect(resMock.status).toBeCalledWith(200);
-        expect(resMock.send).toBeCalledWith(apiResponse);
+        expect(mockResponse.status).toBeCalledWith(200);
+        expect(mockResponse.send).toBeCalledWith(apiResponse);
         expect(Person.find).toHaveBeenCalled();
      });
 })
