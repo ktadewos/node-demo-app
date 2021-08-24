012918b8dd10a334ecfa2af796a8ab9bb6b5490c
kaleab <kaleab.ero@mediaagility.com>
2021-08-24 18:27:27 +0000

    minor fix regarding port value from environment variables

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   db_username: process.env.db_username,
   db_password: process.env.db_password,
   db_name: process.env.db_name,
-  port: process.env.PORT
+  port: process.env.port
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require('./server');
-const {port} = require('./config')
+const {port} = require('./config.js')
 
-app.listen(port, () => console.log('connected'));
+app.listen(port, () => console.log(`Server connected to port ${port}`));
