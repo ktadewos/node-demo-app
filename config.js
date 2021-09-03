0600914a307165e3218205648d709679e744136c
kaleab <kaleab.ero@mediaagility.com>
2021-09-03 14:06:40 +0000

    fix environment variables issue

--- config.js
+++ config.js
@@ -5,3 +5,10 @@ module.exports = {
   test_db_name: 'demo_db_test',
   port: process.env.port
 };
+// module.exports = {
+//  db_username: 'ktadewos',
+//   db_password: '0932355548',
+//   db_name: 'demo_db',
+//   test_db_name: 'demo_db_test',
+//   port: 3000
+// }
