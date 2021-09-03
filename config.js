2025952c9ded7671f4fe236958b4b1de7d7c378b
kaleab <kaleab.ero@mediaagility.com>
2021-09-03 15:18:13 +0000

    clean up code

--- config.js
+++ config.js
@@ -2,13 +2,6 @@ module.exports = {
   db_username: process.env.db_username,
   db_password: process.env.db_password,
   db_name: process.env.db_name,
-  test_db_name: 'demo_db_test',
+  test_db_name: process.env.test_db_name,
   port: process.env.port
 };
-// module.exports = {
-//  db_username: 'ktadewos',
-//   db_password: '0932355548',
-//   db_name: 'demo_db',
-//   test_db_name: 'demo_db_test',
-//   port: 3000
-// }
